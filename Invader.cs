290fba26b185765e6e09d09e93be9bac429220c6
Mark Blanford <markytorture@hotmail.com>
2018-06-15 12:36:48 +0100

    Invader class complete

--- Invader.cs
+++ Invader.cs
@@ -4,20 +4,38 @@ namespace TowerDefence
 {
 
   class Invader {
-
+    //Fields:
     private readonly Path _path;
     //Private field - starts at 0 - all invaders start on the first step
     private int _pathStep = 0;
-    //Auto-property
+
+    //Properties:
+    //Auto-property:
     // public MapLocation Location { get; private set; }
     //Replace property with computed property:
-    public MapLocation Location
+    // public MapLocation Location
+    // {
+    //   get
+    //   {
+    //     return _path.GetLocationAt(_pathStep);
+    //   }
+    // }
+    //Simplify computed property:
+    public MapLocation Location => _path.GetLocationAt(_pathStep);
+
+    public int Health { get; private set; } = 100;
+
+    public bool HasScored
     {
       get
       {
-        return _path.GetLocationAt(_pathStep);
+        return _pathStep >= _path.Length;
       }
     }
+
+    public bool IsNeutralised => Health <= 0;
+
+    public bool IsActive => !(IsNeutralised || HasScored);
 
     //Constructor
     public Invader(Path path)
@@ -25,10 +43,10 @@ namespace TowerDefence
       _path = path;
     }
 
-    public void Move()
-    {
-      _pathStep++;
-    }
+    //Methods:
+    public void Move() => _pathStep++;
+
+    public void DecreaseHealth(int factor) => Health -= factor;
 
     // private MapLocation _location;
 
--- Path.cs
+++ Path.cs
@@ -7,6 +7,8 @@ namespace TowerDefence
 
     private readonly MapLocation[] _path;
 
+    public int Length => _path.Length;
+
     public Path(MapLocation[] path)
     {
       _path = path;
